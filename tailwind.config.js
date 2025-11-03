9816165a0dd8f62002160c811d6e6fdcd19404cd
neonxtech <neonxtech09@gmail.com>
2025-11-03 10:25:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        glass: "rgba(255,255,255,0.06)"
+      }
+    },
   },
   plugins: [],
 };
